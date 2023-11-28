94ca7abcb2961d093ef85e8998809eaa081649b7
ari789 <arielleclapp@gmail.com>
2023-11-27 16:25:02 -0800

    add repl.it link

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # rock-paper-scissors
 Simple rock paper scissors program
 
-Run RockPaperScissors.py on the command line
+### Repl.it
+View and edit [here](https://replit.com/@ari789/rock-paper-scissors#main.py)
+
+### Local Setup
+
+Clone repository
+```
+git clone git@github.com:ari789/rock-paper-scissors.git
+```
+
+Run using python
+```
+pip install colorama
+python RockPaperScissors.py
+```
--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -2,64 +2,75 @@
 
 #imports
 from random import randint
+from colorama import Fore as Color, Style
 
 #declare win counters
 uWins = 0
 cWins = 0
-ties = 0   
-
-#print game instructions
-print("Rock, Paper, Scissors!\n1 = Rock\n2 = Paper\n3 = Scissors\n")
-
+ties = 0
 
 ##This function calculates the winner of rock, paper, scissors
 def winner(u, c):
+  winner = 0
+  if (u == c):
+    print(Color.LIGHTYELLOW_EX + Style.BRIGHT + "\nIt's a tie!")
     winner = 0
-    if(u == c):
-        print("It's a tie!")
-        winner = 0
-    elif(u == 1 and c == 2):
-        print("Paper wraps rock, computer wins!")
-        winner = 2
-    elif(u == 2 and c == 1):
-        print("Paper wraps rock, user wins!")
-        winner = 1
-    elif(u == 1 and c == 3):
-        print("Rock smashes scissors, user wins!")
-        winner = 1
-    elif(u == 3 and c == 1):
-        print("Rock smashes scissors, computer wins!")
-        winner = 2
-    elif(u == 2 and c == 3):
-        print("Scissors cut paper, computer wins!")
-        winner = 2
-    elif(u == 3 and c == 2):
-        print("Scissors cut paper, user wins!")
-        winner = 1
-    return winner
- 
+  elif (u == 1 and c == 2):
+    print(Color.RED + Style.BRIGHT + "\nPaper wraps rock, computer wins!")
+    winner = 2
+  elif (u == 2 and c == 1):
+    print(Color.GREEN + Style.BRIGHT + "\nPaper wraps rock, user wins!")
+    winner = 1
+  elif (u == 1 and c == 3):
+    print(Color.GREEN + Style.BRIGHT + "\nRock smashes scissors, user wins!")
+    winner = 1
+  elif (u == 3 and c == 1):
+    print(Color.RED + Style.BRIGHT + "\nRock smashes scissors, computer wins!")
+    winner = 2
+  elif (u == 2 and c == 3):
+    print(Color.RED + Style.BRIGHT + "Scissors cut paper, computer wins!")
+    winner = 2
+  elif (u == 3 and c == 2):
+    print(Color.GREEN + Style.BRIGHT + "\nScissors cut paper, user wins!")
+    winner = 1
+  return winner
+
 
 #gameplay
 again = 'y'
-while(again.lower() == 'y'):
-    #get choices
-    userChoice = int(input("Enter your choice: "))
-    compChoice = randint(1, 3)
+while (again.lower() == 'y'):
+  #print game instructions
+  print(Style.BRIGHT + '''
+**********************
+Rock, Paper, Scissors!\n
+1 = Rock
+2 = Paper
+3 = Scissors        
+''' + Style.RESET_ALL)
 
-    #calulate wins
-    result = winner(userChoice, compChoice)
-    if(result == 0): ties += 1
-    elif(result == 1): uWins += 1
-    elif(result == 2): cWins += 1
+  #get choices
+  while True:
+    i = input("Enter your choice: ")
+    if (i == "1" or i == "2" or i == "3"):
+      userChoice = int(i)
+      break
+    else:
+      print('Oops! Invalid choice. Try again.')
 
-    #print scoreboard
-    print("Ties: " + str(ties))
-    print("User wins: " + str(uWins))
-    print("Computer wins: " + str(cWins))
+  compChoice = randint(1, 3)
 
-    #ask if continue
-    again = str(input("\nPlay again? (y/n) "))
+  #calulate wins
+  result = winner(userChoice, compChoice)
+  if (result == 0): ties += 1
+  elif (result == 1): uWins += 1
+  elif (result == 2): cWins += 1
+
+  #print scoreboard
+  print(Style.RESET_ALL + "\nTies: " + str(ties))
+  print("User wins: " + str(uWins))
+  print("Computer wins: " + str(cWins))
+
+  #ask if continue
+  again = str(input("\nPlay again? (y/n) "))
 
 print("Bye!")
-          
-    
